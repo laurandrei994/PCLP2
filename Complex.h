2d55e20bf8557858ded196663b728dbacc865c67
Pupaza Andrei Cristian <pandreicristian0109@gmail.com>
2026-03-24 17:10:54 +0200

    update complex class

--- Complex.h
+++ Complex.h
@@ -1,1 +1,29 @@
 #pragma once
+#include <iostream>
+
+class Complex
+{
+private:
+	double real;
+	double imaginar;
+
+public:
+
+	//constructor cu valori implicite
+	Complex(double real = 0, double imaginar = 0);
+
+	//getteri
+	double getReal() const;
+	double getImaginar() const;
+
+	//setteri
+	void setReal(double real);
+	void setImaginar(double imaginar);
+
+	//destructor
+	~Complex();
+
+	//alte functii
+	Complex operator+ (Complex c);
+	friend std::istream& operator>>(std::istream& in, Complex& c);
+};

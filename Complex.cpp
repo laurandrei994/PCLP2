4d4ba3b62bb1503be496346866d1db15a10a58ab
Taz . <nicolas.nitu@student.unitbv.ro>
2026-03-24 18:56:25 +0200

    Update Clase Complexe

--- Complex.cpp
+++ Complex.cpp
@@ -1,1 +1,52 @@
 #include "Complex.h"
+#include <iostream>
+
+
+ 
+Complex::Complex(double real, double imaginar)
+{
+	this->real = real;
+	this->imaginar = imaginar;
+}
+
+double Complex::getReal() const
+{
+	return this->real;
+}
+
+double Complex::getImaginar() const
+{
+	return this->imaginar;
+}
+
+void Complex::setReal(double real)
+{
+	this->real = real;
+}
+
+void Complex::setImaginar(double imaginar)
+{
+	this->imaginar = imaginar;
+}
+
+Complex::~Complex()
+{
+}
+
+Complex Complex::operator+(Complex c)
+{
+	Complex rezultat;
+	rezultat.setReal(this->real + c.getReal());
+	rezultat.setImaginar(this->imaginar + c.getImaginar());
+	return rezultat;
+}
+
+std::istream& operator>>(std::istream& in, Complex& c)
+{
+	std::cout << "Introduceti partea reala: ";
+	in >> c.real;
+	std::cout << "Introduceti partea imaginara: ";
+	in >> c.imaginar;
+	return in;
+	
+}

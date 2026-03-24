409b6c560552b8161c6f54373ea14f38acffe00a
Popa Silvian-Gabriel <gabipopa123@gmail.com>
2026-03-24 17:10:45 +0200

    update complex class cu exercitiul facut impreuna

--- Complex.h
+++ Complex.h
@@ -1,1 +1,22 @@
 #pragma once
+#include <iostream>
+class Complex
+{
+	private:
+	double real;
+	double imaginar;
+public:
+	Complex(double real = 0, double imaginar = 0);
+	double getReal() const;
+	double getImaginar() const;
+	void setReal(double real);
+	void setImaginar(double imaginar);
+	~Complex();
+	Complex operator+(Complex c) const;
+	Complex operator+(int i) const;
+	friend std::istream& operator>>(std::istream& in, Complex& c);
+	//Complex operator-(Complex c) const;
+	Complex operator*(Complex c) const;
+	Complex operator/(Complex c) const;
+	void printComplex() const;
+};
--- main.cpp
+++ main.cpp
@@ -1,10 +1,33 @@
 #include <iostream>
 #include "Complex.h"
+using namespace std;
+
+Complex operator-(Complex c1, Complex c2);
 
 int main()
 {
-	std::cout << "Laborator 6" << std::endl;
-
+	Complex c(5,5);
+	cout << "introduceti un numar complex: " << endl;
+	cin >> c;
+	cout << "partea reala: " << c.getReal() << endl;
+	cout << "partea imaginara: " << c.getImaginar() << endl;
+	Complex rezultat = c + 4;
+	cout << "\nrezultatul adunarii c+c: " << endl 
+		<< "partea reala:"<< rezultat.getReal()<< endl 
+		<< "partea imaginara: "<< rezultat.getImaginar() << endl;
+	rezultat = c - c;
+	cout << "\nrezultatul scaderii c-c: " << endl 
+		<< "partea reala:" << rezultat.getReal() << endl 
+		<< "partea imaginara: " << rezultat.getImaginar() << endl;
 
 	return 0;
+}
+
+
+Complex operator-(Complex c1, Complex c2)
+{
+	Complex rezultat;
+	rezultat.setReal(c1.getReal() - c2.getReal());
+	rezultat.setImaginar(c1.getImaginar() - c2.getImaginar());
+	return rezultat;
 }

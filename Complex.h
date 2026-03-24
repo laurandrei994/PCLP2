98ef3052cc831bc84fa7a16fa8954790c09ec4ea
Maria-Medeea Istratuca <marideeaistra@gmail.com>
2026-03-24 17:10:56 +0200

    Update Complex class

--- Complex.h
+++ Complex.h
@@ -1,1 +1,34 @@
 #pragma once
+#include <iostream>
+
+class Complex
+{
+private:
+	double real;
+	double imaginar;
+
+public:
+	/*Constructor implicit
+		Complex();
+	/Constructor explicit
+		Complex(double real, double imaginar);*/
+
+	//Constructor cu valori implicite (echivalent cu cei 2 de mai sus)
+	Complex(double real = 0, double imaginar = 0);
+
+	//Getteri
+	double getReal() const;
+	double getImaginar() const;
+
+	//Setteri
+	void setReal(double real);
+	void setImaginar(double imaginar);
+
+	//Destructori
+	~Complex();
+
+	//Alte functii
+	Complex operator+(Complex c);
+	Complex operator+(int i);
+	friend std::istream& operator>>(std::istream& in, Complex& c);
+};

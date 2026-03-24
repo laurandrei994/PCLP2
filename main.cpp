05310cc63f0824e2d8a1202274dffa690f950b59
Toia_Codrin <toiacodrin@gmail.com>
2026-03-24 17:13:47 +0200

    update complex class

--- main.cpp
+++ main.cpp
@@ -1,10 +1,34 @@
 #include <iostream>
 #include "Complex.h"
 
+Complex operator-(Complex c1, Complex c2);
+
 int main()
 {
-	std::cout << "Laborator 6" << std::endl;
+	Complex c;
+	std::cout << "introducere un nr complex:" << std::endl;
+	std::cin >> c;
+	std::cout << "partea reala :" << c.getReal() << std::endl;
+	std::cout << "partea imaginare :" << c.getImaginar() << std::endl;
 
+	std::cout << std::endl << "suma :c +c" << std::endl;
+	Complex rezultat;
+	rezultat = c + c;
+	std::cout << "partea reala:" << rezultat.getReal() << std::endl;
+	std::cout << "partea imaginara:" << rezultat.getImaginar() << std::endl;
+
+	std::cout << std::endl << "diferenta :c - c" << std::endl;
+	rezultat = c - c;
+	std::cout << "partea reala:" << rezultat.getReal() << std::endl;
+	std::cout << "partea imaginara:" << rezultat.getImaginar() << std::endl;
 
 	return 0;
+}
+
+Complex operator-(Complex c1, Complex c2)
+{
+	Complex rezultat;
+	rezultat.setReal(c1.getReal() - c2.getReal());
+	rezultat.setImaginar(c1.getImaginar() - c2.getImaginar());
+	return rezultat;
 }

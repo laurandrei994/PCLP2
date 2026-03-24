38555e664f02c09fbda04bcbfa268d2184e7a3e5
Pupaza Andrei Cristian <pandreicristian0109@gmail.com>
2026-03-24 17:28:55 +0200

    update complex

--- Complex.cpp
+++ Complex.cpp
@@ -38,6 +38,16 @@ Complex Complex::operator+(Complex c)
 	return rezultat;
 }
 
+// schimb semnul la imaginar
+Complex Complex::operator!()
+{
+	Complex rezultat;
+	rezultat.setReal(this->real);
+	rezultat.setImaginar(-this->imaginar);
+	return rezultat;
+}
+
+// citesc numarul
 std::istream& operator>>(std::istream& in, Complex& c)
 {
 	std::cout << "introduceti partea reala:";
@@ -45,4 +55,17 @@ std::istream& operator>>(std::istream& in, Complex& c)
 	std::cout << "introduceti partea imaginara:";
 	in >> c.imaginar;
 	return in;
+}
+
+// afisez numarul
+std::ostream& operator<<(std::ostream& out, Complex c)
+{
+	out << c.real;
+
+	if (c.imaginar >= 0)
+		out << " + " << c.imaginar << "i";
+	else
+		out << " - " << -c.imaginar << "i";
+
+	return out;
 }
--- Complex.h
+++ Complex.h
@@ -9,21 +9,29 @@ private:
 
 public:
 
-	//constructor cu valori implicite
+	// constructor
 	Complex(double real = 0, double imaginar = 0);
 
-	//getteri
+	// getteri
 	double getReal() const;
 	double getImaginar() const;
 
-	//setteri
+	// setteri
 	void setReal(double real);
 	void setImaginar(double imaginar);
 
-	//destructor
+	// destructor
 	~Complex();
 
-	//alte functii
+	// adunare
 	Complex operator+ (Complex c);
+
+	// schimb semnul la imaginar
+	Complex operator! ();
+
+	// citire
 	friend std::istream& operator>>(std::istream& in, Complex& c);
+
+	// afisare
+	friend std::ostream& operator<<(std::ostream& out, Complex c);
 };

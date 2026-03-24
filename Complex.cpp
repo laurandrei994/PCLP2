932e0ba601968f3befa25259c27e39920df94575
Samoila Robert <robertor56@yahoo.com>
2026-03-24 20:15:07 +0200

    Lab 6

--- Complex.cpp
+++ Complex.cpp
@@ -1,1 +1,42 @@
 #include "Complex.h"
+
+Complex::Complex(double real, double imaginar)
+{
+	this->real = real;
+	this->imaginar = imaginar;
+}
+
+double Complex::getReal() const
+{
+	return this->real;
+}
+
+double Complex::getImaginar() const
+{
+	return this->imaginar;
+}
+
+void Complex::setReal(double real)
+{
+	this->real = real;
+}
+
+void Complex::setImaginar(double imaginar)
+{
+	this->imaginar = imaginar;
+}
+
+Complex::~Complex()
+{
+}
+
+Complex Complex::operator*(Complex c)
+{
+
+	Complex rezultat;
+	rezultat.setReal(this->real * c.getReal() + this->real * c.getImaginar());
+	rezultat.setImaginar(this->imaginar * c.getImaginar() + this->real * c.getImaginar());
+	return rezultat;
+}
+
+

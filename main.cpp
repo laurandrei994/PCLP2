b366e715afa5294000c6730cf9004a0b77f7d419
Samoila Robert <robertor56@yahoo.com>
2026-03-28 15:24:21 +0200

    Lab 6

--- main.cpp
+++ main.cpp
@@ -1,9 +1,17 @@
 #include <iostream>
 #include "Complex.h"
 
+
+
 int main()
 {
-	std::cout << "Laborator 6" << std::endl;
+	
+	std::cout << "Produs: c * c" << std::endl;
+	
+
+
+	
+
 
 
 	return 0;
